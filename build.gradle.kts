c5ae6ecd0509747dcb014d9567299a5f3e022313
Tatiana <tatianamakovozova.qa@gmail.com>
2024-09-26 14:57:51 +0200

    Простой апи тест

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("io.rest-assured:rest-assured:5.4.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/test/java/org/example/SimpleTest.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+
+public class SimpleTest {
+    @Test
+    public void createUnicorn(){
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+
+        String id = given()
+                .body("{\n" +
+                        "  \"name\": \"Pinky Pie\",\n" +
+                        "  \"color\": \"pink\"\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+        .when()
+                .post("https://crudcrud.com/api/857ed21a992c4387a41b686060c99574/unicorn")
+        .then()
+                .assertThat()
+                .statusCode(201)
+                .body("$", hasKey("_id"))
+        .extract()
+                .path("_id");
+
+
+        given().delete("https://crudcrud.com/api/857ed21a992c4387a41b686060c99574/unicorn/" + id)
+                .then()
+                .assertThat()
+                .statusCode(200);
+
+        given()
+                .get("https://crudcrud.com/api/857ed21a992c4387a41b686060c99574/unicorn/" + id)
+                .then()
+                .assertThat()
+                .statusCode(404);
+
+
+
+
+    }
+
+}
+
